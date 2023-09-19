cb66ad4a83a216c1d6b2a37931ba5621a8917927
huise0ng <shinhuiseong07@gmail.com>
2023-09-19 20:30:59 +0900

    ADD: 23.09.19

---
+++ src/ch09/ex01_2/AccessModifilerOfMember.java
@@ -0,0 +1,12 @@
+package src.ch09.ex01_2;
+
+import ex01_1.A;
+import ex01_2.A;
+
+public class AccessModifilerOfMember {
+
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+    }
+}
