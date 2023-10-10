b4602832cc409f0d8c9b01b4989d0051b2f8edf5
huise0ng <shinhuiseong07@gmail.com>
2023-10-10 22:01:51 +0900

    FIX :: Error fix

--- 23-10-10/src/ex02/Poiymorphism.java
+++ 23-10-10/src/ex02/Poiymorphism.java
@@ -13,19 +13,19 @@ public class Poiymorphism {
         A a3 = new C(); //C는 A이다. 가능하다.
         A a4 = new D(); //D는 A이다. 가능하다. 
         //B가 될 수 있느냐?
-        // B b1 = new A(); // A는 B이다. 불가능하다.
+        //B b1 = new A(); // A는 B이다. 불가능하다.
         B b2 = new B(); // B는 B이다. 가능하다.
         B b3 = new C(); // C는 B이다. 가능하다.
         B b4 = new D(); // D는 B이다. 가능하다.
         //C가 될 수 있느냐?
-        C c1 = new A(); // A는 B이다. 불가능하다.
-        C c2 = new B(); // B는 C이다. 불가능하다.
+        //C c1 = new A(); // A는 B이다. 불가능하다.
+        //C c2 = new B(); // B는 C이다. 불가능하다.
         C c3 = new C(); // C는 C이다. 가능하다.
-        C c4 = new D(); // D는 B다. 불가능하다.
+        //C c4 = new D(); // D는 B다. 불가능하다.
         //D가 될 수 있느냐?
-        D d1 = new A(); // A는 B이다. 불가능하다.
-        D d2 = new B(); // B는 C이다. 불가능하다.
-        D d3 = new C(); // C는 C이다. 불가능하다.
+        //D d1 = new A(); // A는 B이다. 불가능하다.
+        //D d2 = new B(); // B는 C이다. 불가능하다.
+        //D d3 = new C(); // C는 C이다. 불가능하다.
         D d4 = new D(); // D는 B다. 가능하다.
     }
 
