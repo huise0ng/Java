e5af998684b357114dcf32201e0090142b024239
huise0ng <shinhuiseong07@gmail.com>
2023-10-17 20:08:37 +0900

    ADD :: toastlink

--- 23-10-10/src/ex06/Toastlink.java
+++ 23-10-10/src/ex06/Toastlink.java
@@ -1,5 +1,19 @@
 package ex06;
 
-public class Toastlink {
+class A {
+    private int a=3;
+    private int b =4;
+
     
+}
+
+public class Toastlink {
+    public static void main(String[] args) {
+        A a = new A();
+        // hashCode값은 그 객체가 메모리에 머물러 있는 고유 위치 정보
+        System.out.printf("%x \n", a.hashCode());
+        //toString는 그 객체를 나타내는 대표적인 정보이다.
+        // 그정보에 사실 상 필요 없는 해시코드가 들어가 있을 필요가 있나?
+        System.out.println(a.toString());
+    }
 }
