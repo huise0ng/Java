9d69f046caa5af811e4a86f93f8aabea6a89c394
huise0ng <shinhuiseong07@gmail.com>
2023-11-21 17:50:59 +0900

    ADD :: try catch

--- 23-10-10/src/ex003/MultCatch.java
+++ 23-10-10/src/ex003/MultCatch.java
@@ -24,9 +24,18 @@ public class MultCatch {
 //         catch (NumberFormatException e ){
 //             System.out.println("숫자로 바꿀 수 없습니다.");
 //         }
-    System.out.println(3/0);
+// 두가지 예외처리를 한 번의 트라이 캐치를 처리하는 방법
+    try {
+        System.out.println(3/0);
      int num = Integer.parseInt("10"); //이것도 실수 할 수 있는 예외 문장이다.
-/    System.out.println(num);
+     System.out.println(num);
 
     }
+    catch (ArithmeticException | NumberFormatException e){
+        System.out.println("숫자를 0으로 나눌 수 없습니다.");
+    }
+     
+
 }
+
+
