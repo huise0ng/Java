8c3d93945b5b8a76e01cefb58220743fd64a4261
huise0ng <shinhuiseong07@gmail.com>
2023-11-21 17:44:00 +0900

    ADD :: try catch

--- 23-10-10/src/ex003/MultCatch.java
+++ 23-10-10/src/ex003/MultCatch.java
@@ -1,7 +1,32 @@
 package src.ex003;
 
+import java.text.spi.NumberFormatProvider;
+
 public class MultCatch {
     public static void main(String[] args) {
-        System.out.println(3/0);
+//         try {
+//             System.out.println(3/0);
+            
+//         }
+//         catch (ArithmeticException e) {
+//             System.out.println("숫자를 0으로 나눌 수 없습니다.");
+            
+//         }
+//         finally {
+//             System.out.println("프로그램 종료");
+//         }
+
+//         //Integer.parseInt() : ()안에 있는 문자를 숫자로 변화시켜 줌
+//         try {
+//             int num = Integer.parseInt("10"); //이것도 실수 할 수 있는 예외 문장이다.
+//             System.out.println(num);
+//         }
+//         catch (NumberFormatException e ){
+//             System.out.println("숫자로 바꿀 수 없습니다.");
+//         }
+    System.out.println(3/0);
+     int num = Integer.parseInt("10"); //이것도 실수 할 수 있는 예외 문장이다.
+/    System.out.println(num);
+
     }
 }
